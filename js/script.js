ed8767f71192c896c6a2e0b60b74af84bcb042db
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2020-10-14 22:43:06 -0400

    adds search ox and some css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 
 //constants
 
-const API_KEY = '86398dfa2dd7428ab1572271f2048f4c';
-const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
+//const API_KEY = '86398dfa2dd7428ab1572271f2048f4c';
+//const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
 
+
+const key = 'b3aa6bfee94a6040ffc0197c9eb92a1b';
+const id = '52c30435';
+const url = 'https://api.edamam.com/search'
+
+const $input = $('input[type="text"}');
 let recipeData;
 //variables
 
@@ -12,7 +18,23 @@ let recipeData;
 
 
 //event listeners 
+$form.on('submit', handleGetData);
 
 
 
 // functions
+init();
+ function init() {
+     getData();
+ }
+     
+ recipeData = data;
+
+ function getData(detailURL){
+
+     $.ajax(url + '?q=chocolate&app_id=' + id + '&app_key=' + key)
+     .then(data => console.log(data))
+     .then(error => console.log(error))
+
+ };
+ 
