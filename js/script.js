ab9eca3d81cb022228c7254b292f70b9a38e0682
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2020-10-14 21:38:23 -0400

    adds first function

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+//constants
+
+const API_KEY = '86398dfa2dd7428ab1572271f2048f4c';
+const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
+
+let recipeData;
+//variables
+
+
+//cached references
+
+
+//event listeners 
+
+
+
+// functions
