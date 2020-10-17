d7d8bd785bde78fdafce45f2388dfdce7454396b
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2020-10-17 14:42:28 -0400

    adds a few functions

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
 
 //constants
 
-//const API_KEY = '86398dfa2dd7428ab1572271f2048f4c';
-//const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
-
-
 const key = 'b3aa6bfee94a6040ffc0197c9eb92a1b';
 const id = '52c30435';
-const url = 'https://api.edamam.com/search'
+const BASE_URL = 'https://api.edamam.com/search'
 
-const $input = $('input[type="text"}');
-let recipeData;
 //variables
-
+let recipeData, recipeDetail;
 
 //cached references
-
+const $input = $('input[type="text"]');
+const $form = $('form');
 
 //event listeners 
 $form.on('submit', handleGetData);
-
 
 
 // functions
@@ -27,14 +21,18 @@ init();
  function init() {
      getData();
  }
-     
- recipeData = data;
-
+    
+ 
  function getData(detailURL){
-
-     $.ajax(url + '?q=chocolate&app_id=' + id + '&app_key=' + key)
+ const url = detailURL ? detailURL :BASE_URL
+     $.ajax(BASE_URL + '?q=avocado&app_id=' + id + '&app_key=' + key)
      .then(data => console.log(data))
+     
      .then(error => console.log(error))
+  
 
  };
- 
+
+ function handleGetData(event) {
+    event.preventDefault();
+ }
