0aab99fffa46f3e14ae578a075c1bcf2f8cd26c0
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2020-10-20 10:44:59 -0400

    adjusts functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 
 //constants
 
-const key = 'b3aa6bfee94a6040ffc0197c9eb92a1b';
-const id = '52c30435';
+//const key = 'b3aa6bfee94a6040ffc0197c9eb92a1b'; first key
+//const id = '52c30435'; second key
+//const key = '7b5d37d71ff0cd6ec5ff345da2707fd1'; daniel's
+//const id = '4be09ebb'; daniel's
+const key = '4108a648b77483595316cba0cc6ae440';
+const id = '76446193';
 const BASE_URL = 'https://api.edamam.com/search'
 
 //variables
@@ -11,9 +15,9 @@ let recipeData, recipeDetail;
 //cached references
 const $input = $('input[type="text"]');
 const $form = $('form');
-
+const userInput = $input.val();
 //event listeners 
-$form.on('submit', handleGetData);
+$form.on('submit', handleClick);
 
 
 // functions
@@ -25,8 +29,8 @@ init();
  
  function getData(detailURL){
  const url = detailURL ? detailURL :BASE_URL
-     $.ajax(BASE_URL + '?q=avocado&app_id=' + id + '&app_key=' + key)
-     .then(function(data) {
+     $.ajax(BASE_URL + '?q=' + userInput + '&app_id=' + id + '&app_key=' + key)
+      .then(function(data) {
          console.log('Data:', data)
          if(detailURL) {
 
@@ -35,7 +39,7 @@ init();
              
          } else {
              recipeData = data; 
-             render();
+             renderRecipes();
          }
      }, function(error) {
         console.log('Error:', error);
@@ -46,6 +50,12 @@ init();
     event.preventDefault();
  }
  
+function handleClick() {
+    const url = this.dataset.url;
+    getData(url);
+};
 
- function renderRecipes() {
- }
+
+function renderRecipes(){
+   $input.html(`Recipes For: ${recipeData.hits.length}`)
+}
